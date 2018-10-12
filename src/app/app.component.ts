0b1e95c0438c125bf437f984e3a5790adbaf194f
csaba.adam <csaba.adam@shiwaforce.com>
2018-10-12 15:47:47 +0200

    asd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
 	selector: 'app-root',
@@ -9,6 +10,7 @@ import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
 export class AppComponent implements OnInit {
 	genders = ['male', 'female'];
 	signupForm: FormGroup;
+	forbiddenUsernames = ['Chris', 'Anna'];
 
 	ngOnInit() {
 		// create form
@@ -16,16 +18,44 @@ export class AppComponent implements OnInit {
 		this.signupForm = new FormGroup({
 			// azért stringek, hogy minify közben ne rontsa el a form-JS összerendelést
 			'userData': new FormGroup({
-				'username': new FormControl(null, Validators.required),
-				'email': new FormControl(null, [Validators.required, Validators.email])
+				'username': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
+				'email': new FormControl(null, [Validators.required, Validators.email], this.forbiddenEmails)
 			}),
 			'gender': new FormControl('male'),
 			'hobbies': new FormArray([])
+		});
+
+		// form status
+		this.signupForm.valueChanges.subscribe(
+			value => console.log(value)
+		);
+
+		// itt lehet a pendingre ráülni
+		this.signupForm.statusChanges.subscribe(
+			value => console.log(value)
+		);
+
+		// set value ugyanúgy
+		// 	this.signupForm.setValue({
+		// 		'userData': {
+		// 			username: 'asd',
+		// 			email: 'a@b.hu'
+		// 		},
+		// 		gender: 'male',
+		// 		hobbies: []
+		// 	});
+
+		// patch va
+		this.signupForm.patchValue({
+			'userData': {
+				'username': 'makika'
+			}
 		});
 	}
 
 	onSubmit() {
 		console.log(this.signupForm);
+		this.signupForm.reset();
 	}
 
 	// dinamikus mezők
@@ -33,5 +63,31 @@ export class AppComponent implements OnInit {
 		const control = new FormControl(null, Validators.required);
 		// casting
 		(<FormArray>this.signupForm.get('hobbies')).push(control);
+	}
+
+	// custom validator
+	// ha valid, semmivel kell visszatérni
+	forbiddenNames(control: FormControl): {[s: string]: boolean} {
+		if (this.forbiddenUsernames.indexOf(control.value) !== -1) {
+			return {
+				'nameIsForbidden': true
+			};
+		}
+		return null;
+	}
+
+	forbiddenEmails(control: FormControl): Promise<any> | Observable<any> {
+		const promise = new Promise((resolve, reject) => {
+			setTimeout(() => {
+				if (control.value === 'test@test.com') {
+					resolve({
+						'emailIsForbidden': true
+					});
+				} else {
+					resolve(null);
+				}
+			}, 1500);
+		});
+		return promise;
 	}
 }
